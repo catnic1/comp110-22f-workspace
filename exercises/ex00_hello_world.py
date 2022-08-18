441f3850b92b8c24c4b1426e4a8de27df85c4dc6
Nicole Garcia Cortez <catnic@live.unc.edu>
2022-08-18 15:51:49 -0400

    Finished Exercise 00!

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,5 +1,3 @@
-"""My first program for COMP110"""
+"""My first program for COMP110."""
 __author__ = "730328111"
-print("Hello, world my name is Nicole.")
-
-
+print("Hello, world my name is Nicole")
